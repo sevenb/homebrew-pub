4156e5b134b1df81200d2a9855520f1d8ce270a5
Bruno Neves <sevenb@gmail.com>
2016-11-18 23:24:24 -0500

    first commit

---
+++ pub.rb
@@ -0,0 +1,15 @@
+
+class Pub < Formula
+  depends_on "imagemagick"
+  depends_on "jq"
+  desc ""
+  homepage "https://github.com/sevenb/pub/"
+  url "https://github.com/sevenb/pub/archive/v1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "b5534fe55648f36c5bc415dd1190ba166ffe9392e718337ae3d8a9d50f80db67"
+
+
+  def install
+    bin.install "pub"
+  end
+end
